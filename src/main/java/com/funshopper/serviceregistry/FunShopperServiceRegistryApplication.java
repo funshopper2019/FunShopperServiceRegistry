776706ee249374a3aa16f6560c9f58154af92d49
gxk8514 <gabriel_kigundu@homedepot.com>
2019-07-05 16:09:26 -0400

    Init repo

---
+++ src/main/java/com/funshopper/serviceregistry/FunShopperServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.funshopper.serviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunShopperServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunShopperServiceRegistryApplication.class, args);
+	}
+
+}
